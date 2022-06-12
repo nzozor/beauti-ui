e5b86150b0045b32a9768a568af8598e771e97b7
Nick Zozor <nick.zozor@gmail.com>
2022-06-12 11:35:53 +0100

    update to beatuti front end

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-app-beauti'),
+      dir: require('path').join(__dirname, './coverage/beauti-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-app-beauti'`, () => {
+  it(`should have as title 'beauti-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app-beauti');
+    expect(app.title).toEqual('beauti-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app-beauti app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('beauti-frontend app is running!');
   });
 });
