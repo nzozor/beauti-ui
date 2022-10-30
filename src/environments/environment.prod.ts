5f8517fbbff81b3c654ceb7a3c9384ad449555f1
Nick Zozor <nick.zozor@gmail.com>
2022-10-30 17:41:14 +0000

    Update date error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  cmsUrl: 'https://cms.beautiskinclinic.com'
+  cmsUrl: 'https://cms.beautiskinclinic.com',
+  dateApi: "http://167.99.200.71:3006",
 };
