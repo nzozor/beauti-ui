1cd9f18329a6d5c7c4f9e8a7277a0fadfc876d4d
Nicolas Zozor <nick.zozor@gmail.com>
2023-07-31 02:56:07 +0100

    update port prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  cmsUrl: 'https://cms.beautiskinclinic.com',
+  cmsUrl: 'http://localhost:1666',
+  // cmsUrl: 'https://cms.beautiskinclinic.com',
   dateApi: "https://date.tracksdigital.com"
 };
