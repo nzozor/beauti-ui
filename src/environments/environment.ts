006b23c957ef649f44951dafe29b0396c4be61da
Nicolas Zozor <nick.zozor@gmail.com>
2023-07-31 04:23:48 +0100

    update malicious path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  cmsUrl: 'http://167.99.200.71:1339/',
+  cmsUrl: 'http://167.99.200.71:1339',
   // cmsUrl: 'https://cms.beautiskinclinic.com',
   dateApi: "https://date.tracksdigital.com"
 };
