94feec7021eef4e8abe307de50384e92f0465938
Nicolas Zozor <nick.zozor@gmail.com>
2023-08-04 02:40:53 +0100

    ip address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   cmsUrl: 'http://167.99.200.71:1339',
   // cmsUrl: 'https://cms.beautiskinclinic.com',
   dateApi: "https://date.tracksdigital.com",
-  mailService: "http://email.beautiskinclinic.com",
+  // mailService: "http://email.beautiskinclinic.com",
+  mailService: "http://167.99.200.71:3007",
 };
