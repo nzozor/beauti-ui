ae08477e0ff59b30a5a25880cdbab072ea1c19ca
Nicolas Zozor <nick.zozor@gmail.com>
2023-08-22 21:54:23 +0100

    add consultation page

--- route-generator.js
+++ route-generator.js
@@ -3,28 +3,29 @@ const axios = require("axios");
 
 let routes = [];
 axios
-  .get("https://cms.beautiskinclinic.com/treatments")
-  .then((res) => {
-    console.log(`statusCode: ${res.status}`);
-    console.log(res);
-    routes = res;
-    routes = routes.data.map((route) => `/treatments/${route.slug}`);
-    routes.push("/about-us");
-    routes.push("/contact");
-    routes.push("/treatments");
-    routes.push("/");
-    routes = routes.join("\r\n");
-    writeToFile(routes);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+	.get("https://cms.beautiskinclinic.com/treatments")
+	.then((res) => {
+		console.log(`statusCode: ${res.status}`);
+		console.log(res);
+		routes = res;
+		routes = routes.data.map((route) => `/treatments/${route.slug}`);
+		routes.push("/about-us");
+		routes.push("/contact");
+		routes.push("/treatments");
+		routes.push("/consultation");
+		routes.push("/");
+		routes = routes.join("\r\n");
+		writeToFile(routes);
+	})
+	.catch((error) => {
+		console.error(error);
+	});
 
 function writeToFile(routes) {
-  fs.writeFile("./dynamic-routes.txt", routes, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(routes);
-  });
+	fs.writeFile("./dynamic-routes.txt", routes, (err) => {
+		if (err) {
+			console.error(err);
+		}
+		console.log(routes);
+	});
 }
