a459224db04d8ba1f4a63e95cd2b569ad0a42304
Nicolas Zozor <nick.zozor@gmail.com>
2023-09-07 18:25:01 +0100

    update image cropping

--- src/app/modules/treatments/mocks/waxing.ts
+++ src/app/modules/treatments/mocks/waxing.ts
@@ -6,25 +6,26 @@ export const WAX_DATA = [
       {
         treatment: 'Ladies Waxing',
         options: [
-          { key: 'Bikini', value: '£24' },
-          { key: 'Brazilian', value: '£41' },
-          { key: 'Extended Bikini', value: '£32' },
-          { key: 'Full leg', value: '£48' },
-          { key: 'Hollywood', value: '£43' },
-          { key: 'Lower leg', value: '£28' },
-          { key: 'Upper leg', value: '£30' },
+          {key: 'Bikini', value: '£24'},
+          {key: 'Brazilian', value: '£41'},
+          {key: 'Extended Bikini', value: '£32'},
+          {key: 'Full leg', value: '£48'},
+          {key: 'Hollywood', value: '£43'},
+          {key: 'Lower leg', value: '£28'},
+          {key: 'Underarm', value: '£20'},
+          {key: 'Upper leg', value: '£30'},
         ]
       },
       {
         treatment: `Men's Waxing`,
         options: [
-          { key: 'Boyzillian (S&C)', value: '£60' },
-          { key: 'Buttock Crack', value: '£25' },
-          { key: 'Buttocks', value: '£35' },
-          { key: 'Full Back', value: '£40' },
-          { key: 'Full Body', value: '£160' },
-          { key: 'Full Chest', value: '£29' },
-          { key: 'Full Stomach', value: '£25' },
+          {key: 'Boyzillian (S&C)', value: '£60'},
+          {key: 'Buttock Crack', value: '£25'},
+          {key: 'Buttocks', value: '£35'},
+          {key: 'Full Back', value: '£40'},
+          {key: 'Full Body', value: '£160'},
+          {key: 'Full Chest', value: '£29'},
+          {key: 'Full Stomach', value: '£25'},
         ]
       },
 
