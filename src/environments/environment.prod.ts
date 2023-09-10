f27de23141dc63c798c7f19d6dee7ebe1a9123e7
Nicolas Zozor <nick.zozor@gmail.com>
2023-09-10 11:46:06 +0100

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  cmsUrl: 'http://167.99.200.71:1339',
-  // cmsUrl: 'https://cms.beautiskinclinic.com',
+  // cmsUrl: 'http://167.99.200.71:1339',
+  cmsUrl: 'https://cms.beautiskinclinic.com',
   dateApi: "https://date.tracksdigital.com",
   // mailService: "http://email.beautiskinclinic.com",
   mailService: "http://167.99.200.71:3007",
