9e7257769abfd0d4243902961c1e6e718e2a799a
Nicolas Zozor <nick.zozor@gmail.com>
2023-09-11 23:25:28 +0100

    update mail service dns https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   // cmsUrl: 'http://167.99.200.71:1339',
   cmsUrl: 'https://cms.tracksdigital.com',
   dateApi: "https://date.tracksdigital.com",
-  mailService: "http://email.tracksdigital.com",
+  mailService: "https://email.tracksdigital.com",
   // mailService: "http://167.99.200.71:3007",
 };
